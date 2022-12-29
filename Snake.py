ec64e11db1b9f1b4bbfc99d2818870d26877e61e
Firdavs <65146478+Firdavs0636@users.noreply.github.com>
2022-12-29 21:33:16 +0900

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+# Defining constant variables
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -7,6 +8,7 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+# Creating snake class
 class Snake:
 
     def __init__(self):
